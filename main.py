f554657d1d89e443c32c25f7b266e76dd6864af9
moalinium <>
2021-04-04 18:00:22 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,4 @@
+print("Welcome to the band name generator!")
+city = input("What's name of the city you grow up in?\n")
+pet = input("What's ypur pet's name?\n")
+print("Your band name will be " + city + " " + pet)
